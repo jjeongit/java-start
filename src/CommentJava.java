2bc3eb759ae071ac61a28b1bd61c3e4fcf3ec9e0
jjeongit <seul5920@naver.com>
2024-01-23 21:26:19 +0900

    first commit

---
+++ src/CommentJava.java
@@ -0,0 +1,12 @@
+public class CommentJava {
+    public static void main(String[] args) { //psvm 누르면 자동으로 만들어줌
+        System.out.println("sout + enter"); //sout 자동완성
+        System.out.println("hello java1");
+        //System.out.println("hello java2"); 주석인 부분은 모두 무시됨
+
+        /*
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+    }
+}
