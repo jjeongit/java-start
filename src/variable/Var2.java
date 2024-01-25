cb407ee7fbb932c3887c4832f63fd9b2076b9a4e
jjeongit <seul5920@naver.com>
2024-01-25 21:33:31 +0900

    변수 공부

---
+++ src/variable/Var2.java
@@ -0,0 +1,12 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a; // 변수 선언, 그릇개념
+        a = 30; // 변수 초기화, 대입
+
+        System.out.println(a);
+        System.out.println(a);
+        System.out.println(a);
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+        
+        //System.out.println(a);
+
+        /*
+        java: variable a might not have been initialized
+        컴파일 오류
+         */
+        
+    }
+}
